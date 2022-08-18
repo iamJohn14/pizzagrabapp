c8f7512b5addd1f0b4fce0f01a4797ed0e713b8f
iamJohn14 <davidjohn.robles@gmail.com>
2022-08-18 15:27:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  useEffect(() => {
+    document.title = "PizzaGrab";
+  }, []);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Meal/MealsSummary.js
@@ -0,0 +1,16 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Enjoy our specialty edge-to edge pizza!</h2>
+      <p>
+        Bring something new to the table. Traditional brick Ovens x Pizza Made
+        in Italy.
+      </p>
+      <p>Shipping WorldWide. Wood fire ovens. Professional ovens.</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
